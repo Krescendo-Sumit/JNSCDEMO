5488a8dfc4b0b364fdf1f1fcb6e8cf21f69ce2d8
MYTINAUR001 <sssnit@gmail.com>
2023-02-21 19:06:35 -0800

    JNSC Mam Demo

---
+++ app/src/main/java/stundet/db/demo/LayoutDesign.java
@@ -0,0 +1,19 @@
+package stundet.db.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_layout_design);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/stundet/db/demo/Mydemo.java
@@ -0,0 +1,14 @@
+package stundet.db.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mydemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mydemo);
+    }
+}
